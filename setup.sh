2a6dbc0c094fcc885985d69d329d5690aeb2a046
OliveiraEdu <eduardocostaoliveira@gmail.com>
2024-07-23 16:54:16 -0300

    update

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone https://github.com/OliveiraEdu/OpenScience
+cd OpenScience/
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt 
+python -m ipykernel install --user --name=venv
+python -m bash_kernel.install
