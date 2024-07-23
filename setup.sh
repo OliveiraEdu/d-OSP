dbe898ffe653bf1ca8a188e86be01f81922073d6
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2024-07-23 18:20:13 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt 
-python -m ipykernel install --user --name=venv
+git clone https://github.com/OliveiraEdu/OpenScience
+cd OpenScience/
+conda create -n venv
+conda activate venv
+pip install ipykernel
+ipython kernel install --user --name=venv
+pip install bash_kernel
 python -m bash_kernel.install
