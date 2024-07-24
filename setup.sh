ef946689c46036590ec728d4d6e3e7c2308d2ab1
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2024-07-24 09:20:21 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+source venv/bin/activate && pip install -r requirements.txt
 ipython kernel install --user --name=venv
 python -m bash_kernel.install
