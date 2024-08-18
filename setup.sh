907ce0b775cda06617e783f1233397fb46912da0
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2024-08-18 09:07:08 -0300

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-python -m venv venv
-source venv/bin/activate && pip install -r requirements.txt
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 ipython kernel install --user --name=venv
-python -m bash_kernel.install
+jupyter lab --ip=0.0.0.0 --port=8888
