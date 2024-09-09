b9f6a80de03be77fa40903d533fb696379d45ee1
OliveiraEdu <eduardocostaoliveira@gmail.com>
2024-09-09 22:11:44 +0000

    Docker file with Tika is functional, new public key for git sync

--- parse_documents.py
+++ parse_documents.py
@@ -3,9 +3,10 @@ from tika import parser
 def parse_document(filepath):
 
     parsed_document = parser.from_file(filepath)
-    print(parsed_document)
+    # print(parsed_document)
     print(type(parsed_document))
     print(parsed_document.keys()) 
     print(parsed_document['metadata'])  
     print(type(parsed_document['metadata']))
     print(parsed_document['status'],type(parsed_document['status'] ))
+    
--- parsing-JSON.py
+++
@@ -1,5 +0,0 @@
-from tika import parser 
-
-filepath = 'upload/Deep-learning-in-insurance--Accuracy-and-model-int_2023_Expert-Systems-with-.pdf'  # Replace with the path of your file
-parsed_document = parser.from_file(filepath)
-print(parsed_document)
